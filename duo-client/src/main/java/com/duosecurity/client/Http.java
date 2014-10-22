073594f3fa4449654f8562ca2d12a2da8a20b650
Sean O'Neil <oneilse14@gmail.com>
2014-10-22 16:51:40 -0700

    Use OkHttp instead of Apache HttpClient 4.x

--- duo-client/src/main/java/com/duosecurity/client/Http.java
+++ duo-client/src/main/java/com/duosecurity/client/Http.java
@@ -1,58 +1,50 @@
 package com.duosecurity.client;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import com.squareup.okhttp.Headers;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 import java.io.UnsupportedEncodingException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
 
-import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.conn.params.ConnRoutePNames;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.message.HeaderGroup;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
-import org.apache.http.params.HttpConnectionParams;
 import org.json.JSONObject;
 
 public class Http {
     private String method;
     private String host;
     private String uri;
-    private HeaderGroup headers;
-    private ArrayList<NameValuePair> params;
-    private HttpHost proxy;
+    private Headers.Builder headers;
+    Map<String, String> params = new HashMap<String, String>();
+    private Proxy proxy;
     private int timeout = 3 * 60 * 1000;
 
     public static SimpleDateFormat RFC_2822_DATE_FORMAT
         = new SimpleDateFormat("EEE', 'dd' 'MMM' 'yyyy' 'HH:mm:ss' 'Z",
                                Locale.US);
 
+    public static MediaType FORM_ENCODED = MediaType.parse("application/x-www-form-urlencoded");
+
     public Http(String in_method, String in_host, String in_uri) {
         method = in_method.toUpperCase();
         host = in_host;
         uri = in_uri;
 
-        headers = new HeaderGroup();
-        addHeader("Host", host);
+        headers = new Headers.Builder();
+        headers.add("Host", host);
 
-        params = new ArrayList<NameValuePair>();
         proxy = null;
     }
 
@@ -73,50 +65,50 @@ public class Http {
     }
 
     public String executeRequestRaw() throws Exception {
-        HttpResponse response = executeHttpRequest();
-        // parse response
-        InputStream stream = response.getEntity().getContent();
-        String buf = streamToString(stream);
-        return buf;
+        Response response = executeHttpRequest();
+        return response.body().string();
     }
 
-    public HttpResponse executeHttpRequest() throws Exception {
+    public Response executeHttpRequest() throws Exception {
       String url = "https://" + host + uri;
       String queryString = createQueryString();
 
-      HttpRequestBase request;
-      if (method.equals("GET") || method.equals("DELETE")) {
+      Request.Builder builder = new Request.Builder();
+      if (method.equals("POST")) {
+        builder.post(RequestBody.create(FORM_ENCODED, queryString));
+      } else if (method.equals("PUT")) {
+        builder.put(RequestBody.create(FORM_ENCODED, queryString));
+      } else if (method.equals("GET")) {
         if (queryString.length() > 0) {
           url += "?" + queryString;
         }
-      }
-
-      if (method.equals("GET")) {
-        request = new HttpGet(url);
-      } else if (method.equals("POST")) { // or PUT (currently unused)
-        HttpEntityEnclosingRequestBase ee_request = new HttpPost(url);
-        ee_request.setEntity(new UrlEncodedFormEntity(params));
-        request = ee_request;
-      } else if (method.equals("DELETE")) {
-        request = new HttpDelete(url);
+        builder.get();
+      } else if(method.equals("DELETE")) {
+        if (queryString.length() > 0) {
+          url += "?" + queryString;
+        }
+        builder.delete();
       } else {
         throw new UnsupportedOperationException("Unsupported method: "
             + method);
       }
 
+      Request request = builder.url(url)
+          .build();
+
       // Set up client.
-      HttpClient httpclient = new DefaultHttpClient();
+      OkHttpClient httpclient = new OkHttpClient();
       if (proxy != null) {
-        httpclient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY,
-            proxy);
+        httpclient.setProxy(proxy);
       }
 
-      HttpConnectionParams.setConnectionTimeout(httpclient.getParams(), timeout);
-      HttpConnectionParams.setSoTimeout(httpclient.getParams(), timeout);
-
+      httpclient.setConnectTimeout(timeout, TimeUnit.SECONDS);
+      httpclient.setWriteTimeout(timeout, TimeUnit.SECONDS);
+      httpclient.setReadTimeout(timeout, TimeUnit.SECONDS);
       // finish and execute request
-      request.setHeaders(headers.getAllHeaders());
-      return httpclient.execute(request);
+      builder.headers(headers.build());
+      return httpclient.newCall(builder.build())
+          .execute();
     }
 
     public void signRequest(String ikey, String skey)
@@ -155,16 +147,15 @@ public class Http {
     }
 
     public void addHeader(String name, String value) {
-        headers.addHeader(new BasicHeader(name, value));
+        headers.add(name, value);
     }
 
     public void addParam(String name, String value) {
-        params.add(new BasicNameValuePair(name, value));
+      params.put(name, value);
     }
 
-
     public void setProxy(String host, int port) {
-        proxy = new HttpHost(host, port, "http");
+        proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(host, port));
     }
 
     protected String canonRequest(String date, int sig_version)
@@ -181,59 +172,31 @@ public class Http {
         return canon;
     }
 
-    private String streamToString(InputStream stream) {
-        BufferedReader reader = new BufferedReader(
-                                                   new InputStreamReader(stream));
-        StringBuilder sb = new StringBuilder();
-        String line = null;
-
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                stream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return sb.toString();
-    }
-
     private String createQueryString()
-      throws UnsupportedEncodingException {
-        ArrayList<String> args = new ArrayList<String>();
-        ArrayList<String> keys = new ArrayList<String>();
+        throws UnsupportedEncodingException {
+      ArrayList<String> args = new ArrayList<String>();
+      ArrayList<String> keys = new ArrayList<String>();
 
-        for (NameValuePair pair : params) {
-            keys.add(pair.getName());
-        }
+      for (String key : params.keySet()) {
+        keys.add(key);
+      }
 
-        Collections.sort(keys);
+      Collections.sort(keys);
 
-        for (String key : keys) {
-            for (NameValuePair pair : params) {
-                if (key.equals(pair.getName())) {
-                    String name = URLEncoder
-                        .encode(pair.getName(), "UTF-8")
-                        .replace("+", "%20")
-                        .replace("*", "%2A")
-                        .replace("%7E", "~");
-                    String value = URLEncoder
-                        .encode(pair.getValue(), "UTF-8")
-                        .replace("+", "%20")
-                        .replace("*", "%2A")
-                        .replace("%7E", "~");
-                    args.add(name + "=" + value);
-                    break;
-                }
-            }
-        }
+      for (String key : keys) {
+        String name = URLEncoder
+            .encode(key, "UTF-8")
+            .replace("+", "%20")
+            .replace("*", "%2A")
+            .replace("%7E", "~");
+        String value = URLEncoder
+            .encode(params.get(key), "UTF-8")
+            .replace("+", "%20")
+            .replace("*", "%2A")
+            .replace("%7E", "~");
+        args.add(name + "=" + value);
+      }
 
-        return Util.join(args.toArray(), "&");
+      return Util.join(args.toArray(), "&");
     }
 }
