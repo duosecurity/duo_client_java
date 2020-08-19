f454f6e33ca6affae7fba555203fbac6ae2d15d1
Briar Scott <bscott@bscott-14399.local>
2020-08-19 15:11:07 -0400

    Adding a linter

--- duo-client/src/main/java/com/duosecurity/client/Http.java
+++ duo-client/src/main/java/com/duosecurity/client/Http.java
@@ -12,225 +12,271 @@ import java.net.Proxy;
 import java.net.URLEncoder;
 import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
-
 import org.json.JSONObject;
 
 public class Http {
-    public final static int BACKOFF_FACTOR = 2;
-    public final static int INITIAL_BACKOFF_MS = 1000;
-    public final static int MAX_BACKOFF_MS = 32000;
-    public final static int DEFAULT_TIMEOUT_SECS = 60;
-    private final static int RATE_LIMIT_ERROR_CODE = 429;
+  public static final int BACKOFF_FACTOR = 2;
+  public static final int INITIAL_BACKOFF_MS = 1000;
+  public static final int MAX_BACKOFF_MS = 32000;
+  public static final int DEFAULT_TIMEOUT_SECS = 60;
+  private static final int RATE_LIMIT_ERROR_CODE = 429;
 
-    public final static String HmacSHA1 = "HmacSHA1";
-    public final static String HmacSHA512 = "HmacSHA512";
-    public final static String UserAgentString = "Duo API Java/0.3.0";
+  public static final String HmacSHA1 = "HmacSHA1";
+  public static final String HmacSHA512 = "HmacSHA512";
+  public static final String UserAgentString = "Duo API Java/0.3.0";
 
-    private String method;
-    private String host;
-    private String uri;
-    private String signingAlgorithm;
-    private Headers.Builder headers;
-    Map<String, String> params = new HashMap<String, String>();
-    private Random random = new Random();
-    private OkHttpClient httpClient;
+  private String method;
+  private String host;
+  private String uri;
+  private String signingAlgorithm;
+  private Headers.Builder headers;
+  Map<String, String> params = new HashMap<String, String>();
+  private Random random = new Random();
+  private OkHttpClient httpClient;
 
-    public static SimpleDateFormat RFC_2822_DATE_FORMAT
-        = new SimpleDateFormat("EEE', 'dd' 'MMM' 'yyyy' 'HH:mm:ss' 'Z",
-                               Locale.US);
+  public static SimpleDateFormat RFC_2822_DATE_FORMAT
+      = new SimpleDateFormat("EEE', 'dd' 'MMM' 'yyyy' 'HH:mm:ss' 'Z", Locale.US);
 
-    public static MediaType FORM_ENCODED = MediaType.parse("application/x-www-form-urlencoded");
+  public static MediaType FORM_ENCODED = MediaType.parse("application/x-www-form-urlencoded");
 
-    public Http(String in_method, String in_host, String in_uri) {
-        this(in_method, in_host, in_uri, DEFAULT_TIMEOUT_SECS);
+  public Http(String inMethod, String inHost, String inUri) {
+    this(inMethod, inHost, inUri, DEFAULT_TIMEOUT_SECS);
+  }
+
+  /**
+   * Http constructor.
+   *
+   * @param inMethod    The method for the http request
+   * @param inHost      The api host provided by Duo and found in the Duo admin panel
+   * @param inUri       The endpoint for the request
+   * @param timeout     The timeout for the http request
+   */
+  public Http(String inMethod, String inHost, String inUri, int timeout) {
+    method = inMethod.toUpperCase();
+    host = inHost;
+    uri = inUri;
+    signingAlgorithm = "HmacSHA1";
+
+    headers = new Headers.Builder();
+    headers.add("Host", host);
+    headers.add("user-agent", UserAgentString);
+
+    httpClient = new OkHttpClient();
+    httpClient.setConnectTimeout(timeout, TimeUnit.SECONDS);
+    httpClient.setWriteTimeout(timeout, TimeUnit.SECONDS);
+    httpClient.setReadTimeout(timeout, TimeUnit.SECONDS);
+  }
+
+  /**
+   * Executes JSON request.
+   *
+   * @return            The result of the JSON request
+   *
+   * @throws Exception  If the result was not OK
+   */
+  public Object executeJSONRequest() throws Exception {
+    JSONObject result = new JSONObject(executeRequestRaw());
+    if (! result.getString("stat").equals("OK")) {
+      throw new Exception("Duo error code ("
+                          + result.getInt("code")
+                          + "): "
+                          + result.getString("message"));
+    }
+    return result;
+  }
+
+  public String executeRequestRaw() throws Exception {
+    Response response = executeHttpRequest();
+    return response.body().string();
+  }
+
+  /**
+   * Creates and executes a HTTP request.
+   *
+   * @return                              The result of the HTTP request
+   *
+   * @throws UnsupportedEncodingException For http methods that are not supported
+   */
+  public Response executeHttpRequest() throws Exception {
+    String url = "https://" + host + uri;
+    String queryString = createQueryString();
+
+    Request.Builder builder = new Request.Builder();
+    if (method.equals("POST")) {
+      builder.post(RequestBody.create(FORM_ENCODED, queryString));
+    } else if (method.equals("PUT")) {
+      builder.put(RequestBody.create(FORM_ENCODED, queryString));
+    } else if (method.equals("GET")) {
+      if (queryString.length() > 0) {
+        url += "?" + queryString;
+      }
+      builder.get();
+    } else if (method.equals("DELETE")) {
+      if (queryString.length() > 0) {
+        url += "?" + queryString;
+      }
+      builder.delete();
+    } else {
+      throw new UnsupportedOperationException("Unsupported method: " + method);
     }
 
-    public Http(String in_method, String in_host, String in_uri, int timeout) {
-        method = in_method.toUpperCase();
-        host = in_host;
-        uri = in_uri;
-        signingAlgorithm = "HmacSHA1";
+    // finish and execute request
+    Request request = builder.headers(headers.build()).url(url).build();
+    return executeRequest(request);
+  }
 
-        headers = new Headers.Builder();
-        headers.add("Host", host);
-        headers.add("user-agent", UserAgentString);
+  public Object executeRequest() throws Exception {
+    JSONObject result = (JSONObject)executeJSONRequest();
+    return result.get("response");
+  }
 
-        httpClient = new OkHttpClient();
-        httpClient.setConnectTimeout(timeout, TimeUnit.SECONDS);
-        httpClient.setWriteTimeout(timeout, TimeUnit.SECONDS);
-        httpClient.setReadTimeout(timeout, TimeUnit.SECONDS);
-    }
-
-    public Object executeRequest() throws Exception {
-        JSONObject result = (JSONObject)executeJSONRequest();
-        return result.get("response");
-    }
-
-    public Object executeJSONRequest() throws Exception {
-        JSONObject result = new JSONObject(executeRequestRaw());
-        if (! result.getString("stat").equals("OK")) {
-            throw new Exception("Duo error code ("
-                                + result.getInt("code")
-                                + "): "
-                                + result.getString("message"));
-        }
-        return result;
-    }
-
-    public String executeRequestRaw() throws Exception {
-        Response response = executeHttpRequest();
-        return response.body().string();
-    }
-
-    public Response executeHttpRequest() throws Exception {
-      String url = "https://" + host + uri;
-      String queryString = createQueryString();
-
-      Request.Builder builder = new Request.Builder();
-      if (method.equals("POST")) {
-        builder.post(RequestBody.create(FORM_ENCODED, queryString));
-      } else if (method.equals("PUT")) {
-        builder.put(RequestBody.create(FORM_ENCODED, queryString));
-      } else if (method.equals("GET")) {
-        if (queryString.length() > 0) {
-          url += "?" + queryString;
-        }
-        builder.get();
-      } else if(method.equals("DELETE")) {
-        if (queryString.length() > 0) {
-          url += "?" + queryString;
-        }
-        builder.delete();
-      } else {
-        throw new UnsupportedOperationException("Unsupported method: "
-            + method);
+  private Response executeRequest(Request request) throws Exception {
+    long backoffMs = INITIAL_BACKOFF_MS;
+    while (true) {
+      Response response = httpClient.newCall(request).execute();
+      if (response.code() != RATE_LIMIT_ERROR_CODE || backoffMs > MAX_BACKOFF_MS) {
+        return response;
       }
 
-      // finish and execute request
-      Request request = builder.headers(headers.build()).url(url).build();
-      return executeRequest(request);
+      sleep(backoffMs + random.nextInt(1000));
+      backoffMs *= BACKOFF_FACTOR;
     }
+  }
 
-    private Response executeRequest(Request request) throws Exception {
-        long backoffMs = INITIAL_BACKOFF_MS;
-        while (true) {
-            Response response = httpClient.newCall(request).execute();
-            if (response.code() != RATE_LIMIT_ERROR_CODE || backoffMs > MAX_BACKOFF_MS) {
-                return response;
-            }
+  protected void sleep(long ms) throws Exception {
+    Thread.sleep(ms);
+  }
 
-            sleep(backoffMs + random.nextInt(1000));
-            backoffMs *= BACKOFF_FACTOR;
-        }
-    }
-
-    protected void sleep(long ms) throws Exception {
-        Thread.sleep(ms);
-    }
-
-    public void signRequest(String ikey, String skey)
+  public void signRequest(String ikey, String skey)
       throws UnsupportedEncodingException {
-        signRequest(ikey, skey, 2);
-    }
+    signRequest(ikey, skey, 2);
+  }
 
-    public void signRequest(String ikey, String skey, int sig_version)
+  /**
+   * Signs Duo request.
+   *
+   * @param ikey        Integration key provided by Duo and found in the admin panel
+   * @param skey        Secret key provided by Duo and found in the admin panel
+   * @param sigVersion  The version of signature used
+   *
+   * @throws UnsupportedEncodingException For unsupported encodings
+   */
+  public void signRequest(String ikey, String skey, int sigVersion)
       throws UnsupportedEncodingException {
-        String date = formatDate(new Date());
-        String canon = canonRequest(date, sig_version);
-        String sig = signHMAC(skey, canon);
+    String date = formatDate(new Date());
+    String canon = canonRequest(date, sigVersion);
+    String sig = signHMAC(skey, canon);
 
-        String auth = ikey + ":" + sig;
-        String header = "Basic " + Base64.encodeBytes(auth.getBytes());
-        addHeader("Authorization", header);
-        if (sig_version == 2) {
-            addHeader("Date", date);
-        }
+    String auth = ikey + ":" + sig;
+    String header = "Basic " + Base64.encodeBytes(auth.getBytes());
+    addHeader("Authorization", header);
+    if (sigVersion == 2) {
+      addHeader("Date", date);
     }
+  }
 
-    protected String signHMAC(String skey, String msg) {
-        try {
-            byte[] sig_bytes = Util.hmac(signingAlgorithm,
-                                         skey.getBytes(),
-                                         msg.getBytes());
-            String sig = Util.bytes_to_hex(sig_bytes);
-            return sig;
-        } catch (Exception e) {
-            return "";
-        }
+  protected String signHMAC(String skey, String msg) {
+    try {
+      byte[] sigBytes = Util.hmac(signingAlgorithm,
+                                   skey.getBytes(),
+                                   msg.getBytes());
+      String sig = Util.bytes_to_hex(sigBytes);
+      return sig;
+    } catch (Exception e) {
+      return "";
     }
+  }
 
-    private String formatDate(Date date) {
-        // Could use ThreadLocal or a pool of format objects instead
-        // depending on the needs of the application.
-        synchronized (RFC_2822_DATE_FORMAT) {
-            return RFC_2822_DATE_FORMAT.format(date);
-        }
+  private String formatDate(Date date) {
+    // Could use ThreadLocal or a pool of format objects instead
+    // depending on the needs of the application.
+    synchronized (RFC_2822_DATE_FORMAT) {
+      return RFC_2822_DATE_FORMAT.format(date);
     }
+  }
 
-    public void addHeader(String name, String value) {
-        headers.add(name, value);
-    }
+  public void addHeader(String name, String value) {
+    headers.add(name, value);
+  }
 
-    public void addParam(String name, String value) {
-      params.put(name, value);
-    }
+  public void addParam(String name, String value) {
+    params.put(name, value);
+  }
 
-    public void setProxy(String host, int port) {
-        this.httpClient.setProxy(
-            new Proxy(Proxy.Type.HTTP, new InetSocketAddress(host, port))
-        );
-    }
+  /**
+   * Creates a new proxy.
+   *
+   * @param host    The proxy host
+   * @param port    The port of the proxy
+   */
+  public void setProxy(String host, int port) {
+    this.httpClient.setProxy(
+        new Proxy(Proxy.Type.HTTP, new InetSocketAddress(host, port))
+    );
+  }
 
-    public void setSigningAlgorithm(String algorithm)
+  /**
+   * Set Signing Algorithm.
+   *
+   * @param algorithm   The algorith used for signing
+   *
+   * @throws NoSuchAlgorithmException For algorithms that are not HmacSHA1 or HmacSHA512
+   */
+  public void setSigningAlgorithm(String algorithm)
       throws NoSuchAlgorithmException {
-        if (algorithm != HmacSHA1 && algorithm != HmacSHA512) {
-            throw new NoSuchAlgorithmException(algorithm);
-        }
-        signingAlgorithm = algorithm;
+    if (algorithm != HmacSHA1 && algorithm != HmacSHA512) {
+      throw new NoSuchAlgorithmException(algorithm);
     }
+    signingAlgorithm = algorithm;
+  }
 
-    protected String canonRequest(String date, int sig_version)
+  protected String canonRequest(String date, int sigVersion)
       throws UnsupportedEncodingException {
-        String canon = "";
-        if (sig_version == 2) {
-            canon += date + "\n";
-        }
-        canon += method.toUpperCase() + "\n";
-        canon += host.toLowerCase() + "\n";
-        canon += uri + "\n";
-        canon += createQueryString();
+    String canon = "";
+    if (sigVersion == 2) {
+      canon += date + "\n";
+    }
+    canon += method.toUpperCase() + "\n";
+    canon += host.toLowerCase() + "\n";
+    canon += uri + "\n";
+    canon += createQueryString();
 
-        return canon;
+    return canon;
+  }
+
+  private String createQueryString()
+      throws UnsupportedEncodingException {
+    ArrayList<String> args = new ArrayList<String>();
+    ArrayList<String> keys = new ArrayList<String>();
+
+    for (String key : params.keySet()) {
+      keys.add(key);
     }
 
-    private String createQueryString()
-        throws UnsupportedEncodingException {
-      ArrayList<String> args = new ArrayList<String>();
-      ArrayList<String> keys = new ArrayList<String>();
+    Collections.sort(keys);
 
-      for (String key : params.keySet()) {
-        keys.add(key);
-      }
-
-      Collections.sort(keys);
-
-      for (String key : keys) {
-        String name = URLEncoder
-            .encode(key, "UTF-8")
-            .replace("+", "%20")
-            .replace("*", "%2A")
-            .replace("%7E", "~");
-        String value = URLEncoder
-            .encode(params.get(key), "UTF-8")
-            .replace("+", "%20")
-            .replace("*", "%2A")
-            .replace("%7E", "~");
-        args.add(name + "=" + value);
-      }
-
-      return Util.join(args.toArray(), "&");
+    for (String key : keys) {
+      String name = URLEncoder
+          .encode(key, "UTF-8")
+          .replace("+", "%20")
+          .replace("*", "%2A")
+          .replace("%7E", "~");
+      String value = URLEncoder
+          .encode(params.get(key), "UTF-8")
+          .replace("+", "%20")
+          .replace("*", "%2A")
+          .replace("%7E", "~");
+      args.add(name + "=" + value);
     }
+
+    return Util.join(args.toArray(), "&");
+  }
 }
--- duo-example-admin/src/main/java/com/duosecurity/example/DuoAdmin.java
+++ duo-example-admin/src/main/java/com/duosecurity/example/DuoAdmin.java
@@ -6,13 +6,10 @@ package com.duosecurity.example;
  * Documentation: http://www.duosecurity.com/docs/adminapi
  */
 
+import com.duosecurity.client.Http;
 import java.util.Iterator;
-import java.util.regex.Pattern;
 import java.util.regex.Matcher;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
+import java.util.regex.Pattern;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.HelpFormatter;
@@ -20,154 +17,156 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
-
-import com.duosecurity.client.Http;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class DuoAdmin {
-    private static String proxy_host = null;
-    private static int proxy_port = 0;
-    public static void main(String[] args) {
-        System.out.println("Duo Admin Demo");
+  private static String proxy_host = null;
+  private static int proxy_port = 0;
 
-        Options options = new Options();
-        Option opt;
-        opt = new Option("host", true, "API hostname (required)");
-        opt.setRequired(true);
-        options.addOption(opt);
-        opt = new Option("ikey", true, "Admin API integration key (required)");
-        opt.setRequired(true);
-        options.addOption(opt);
-        opt = new Option("skey", true, "Secret key (required)");
-        opt.setRequired(true);
-        options.addOption(opt);
-        opt = new Option("proxy", true, "host:port for HTTPS CONNECT proxy");
-        opt.setRequired(false);
-        options.addOption(opt);
-        options.addOption("help", false, "Print this message");
+  /**
+   * The main function for the DuoAdmin example that will get users and authentication attempts.
+   *
+   * @param args    The command line arguments
+   */
+  public static void main(String[] args) {
+    System.out.println("Duo Admin Demo");
 
-        CommandLineParser parser = new PosixParser();
-        HelpFormatter formatter = new HelpFormatter();
-        CommandLine cmd;
-        try {
-            cmd = parser.parse(options, args);
-        }
-        catch (ParseException parseException) {
-            System.err.println(parseException.getMessage());
-            formatter.printHelp("DuoAdmin", options);
-            System.exit(1);
-            return;
-        }
+    Options options = new Options();
+    Option opt;
+    opt = new Option("host", true, "API hostname (required)");
+    opt.setRequired(true);
+    options.addOption(opt);
+    opt = new Option("ikey", true, "Admin API integration key (required)");
+    opt.setRequired(true);
+    options.addOption(opt);
+    opt = new Option("skey", true, "Secret key (required)");
+    opt.setRequired(true);
+    options.addOption(opt);
+    opt = new Option("proxy", true, "host:port for HTTPS CONNECT proxy");
+    opt.setRequired(false);
+    options.addOption(opt);
+    options.addOption("help", false, "Print this message");
 
-        if (cmd.hasOption("help")) {
-            formatter.printHelp("DuoAdmin", options);
-            System.exit(0);
-        }
-
-        if (cmd.hasOption("proxy")) {
-            Pattern p = Pattern.compile("^([^:]+):(\\d{1,5})$");
-            Matcher m = p.matcher(cmd.getOptionValue("proxy"));
-            if (m.find()) {
-                proxy_host = m.group(1);
-                proxy_port = Integer.parseInt(m.group(2));
-            }
-            else {
-                System.out.println("Invalid proxy.");
-                System.exit(1);
-                return;
-            }
-        }
-
-        getAuthenticationAttempts(cmd);
-        getUsersWithPaging(cmd);
-
-        System.out.println("Done with Admin API demo.");
+    CommandLineParser parser = new PosixParser();
+    HelpFormatter formatter = new HelpFormatter();
+    CommandLine cmd;
+    try {
+      cmd = parser.parse(options, args);
+    } catch (ParseException parseException) {
+      System.err.println(parseException.getMessage());
+      formatter.printHelp("DuoAdmin", options);
+      System.exit(1);
+      return;
     }
 
-    private static void getAuthenticationAttempts(CommandLine cmd) {
-        JSONObject result = null;
-        try {
-            // Prepare request.
-            Http request = new Http("GET",
-                                    cmd.getOptionValue("host"),
-                                    "/admin/v1/info/authentication_attempts");
-            request.signRequest(cmd.getOptionValue("ikey"),
-                                cmd.getOptionValue("skey"));
-
-            // Use proxy if one was specified.
-            if (proxy_host != null) {
-                request.setProxy(proxy_host, proxy_port);
-            }
-
-            // Send the request to Duo and parse the response.
-            result = (JSONObject)request.executeRequest();
-            System.out.println("mintime = " + result.getInt("mintime"));
-            System.out.println("maxtime = " + result.getInt("maxtime"));
-
-            JSONObject attempts
-                = result.getJSONObject("authentication_attempts");
-            Iterator<?> keys = attempts.keys();
-            while (keys.hasNext()) {
-                String key = (String)keys.next();
-                System.out.println(key + " count = " + attempts.getInt(key));
-            }
-        }
-        catch (Exception e) {
-            System.out.println("error making request");
-            System.out.println(e.toString());
-        }
+    if (cmd.hasOption("help")) {
+      formatter.printHelp("DuoAdmin", options);
+      System.exit(0);
     }
 
-    private static void getUsersWithPaging(CommandLine cmd) {
-        JSONObject result;
-        JSONArray response;
-        JSONObject metadata;
-        try {
-            // Prepare request.
-            Http request = new Http("GET", cmd.getOptionValue("host"), "/admin/v1/users");
-            String limit = "10";
-            request.addParam("offset", "0");
-            request.addParam("limit", limit);
-            request.signRequest(cmd.getOptionValue("ikey"), cmd.getOptionValue("skey"));
-
-            // Use proxy if one was specified.
-            if (proxy_host != null) {
-                request.setProxy(proxy_host, proxy_port);
-            }
-
-            int offset = 0;
-            boolean hasMoreUsers = true;
-            while (hasMoreUsers) {
-                // Send the request to Duo and parse the response.
-                System.out.println("Fetching " + limit + " users at offset " + offset);
-
-                result = (JSONObject) request.executeJSONRequest();
-                response = result.getJSONArray("response");
-                metadata = result.getJSONObject("metadata");
-
-                if (!metadata.isNull("next_offset")) {
-                    offset = metadata.getInt("next_offset");
-
-                    request = new Http("GET", cmd.getOptionValue("host"), "/admin/v1/users");
-                    request.addParam("offset", Integer.toString(offset));
-                    request.addParam("limit", limit);
-                    request.signRequest(cmd.getOptionValue("ikey"), cmd.getOptionValue("skey"));
-
-                    System.out.println("More to fetch: next_offset = " + offset);
-                } else {
-                    hasMoreUsers = false;
-                    System.out.println("Fetch user request done.");
-                }
-
-                // iterate users and print them
-                for (int i = 0; i < response.length(); i++) {
-                    JSONObject user = response.getJSONObject(i);
-                    System.out.println("Fetched user: " + user.get("username"));
-                }
-            }
-        }
-        catch (Exception e) {
-            System.out.println("error making request");
-            System.out.println(e.toString());
-        }
+    if (cmd.hasOption("proxy")) {
+      Pattern p = Pattern.compile("^([^:]+):(\\d{1,5})$");
+      Matcher m = p.matcher(cmd.getOptionValue("proxy"));
+      if (m.find()) {
+        proxy_host = m.group(1);
+        proxy_port = Integer.parseInt(m.group(2));
+      } else {
+        System.out.println("Invalid proxy.");
+        System.exit(1);
+        return;
+      }
     }
+
+    getAuthenticationAttempts(cmd);
+    getUsersWithPaging(cmd);
+
+    System.out.println("Done with Admin API demo.");
+  }
+
+  private static void getAuthenticationAttempts(CommandLine cmd) {
+    JSONObject result = null;
+    try {
+      // Prepare request.
+      Http request = new Http("GET",
+                              cmd.getOptionValue("host"),
+                              "/admin/v1/info/authentication_attempts");
+      request.signRequest(cmd.getOptionValue("ikey"),
+                          cmd.getOptionValue("skey"));
+
+      // Use proxy if one was specified.
+      if (proxy_host != null) {
+        request.setProxy(proxy_host, proxy_port);
+      }
+
+      // Send the request to Duo and parse the response.
+      result = (JSONObject)request.executeRequest();
+      System.out.println("mintime = " + result.getInt("mintime"));
+      System.out.println("maxtime = " + result.getInt("maxtime"));
+
+      JSONObject attempts
+          = result.getJSONObject("authentication_attempts");
+      Iterator<?> keys = attempts.keys();
+      while (keys.hasNext()) {
+        String key = (String)keys.next();
+        System.out.println(key + " count = " + attempts.getInt(key));
+      }
+    } catch (Exception e) {
+      System.out.println("error making request");
+      System.out.println(e.toString());
+    }
+  }
+
+  private static void getUsersWithPaging(CommandLine cmd) {
+    JSONObject result;
+    JSONArray response;
+    JSONObject metadata;
+    try {
+      // Prepare request.
+      Http request = new Http("GET", cmd.getOptionValue("host"), "/admin/v1/users");
+      String limit = "10";
+      request.addParam("offset", "0");
+      request.addParam("limit", limit);
+      request.signRequest(cmd.getOptionValue("ikey"), cmd.getOptionValue("skey"));
+
+      // Use proxy if one was specified.
+      if (proxy_host != null) {
+        request.setProxy(proxy_host, proxy_port);
+      }
+
+      int offset = 0;
+      boolean hasMoreUsers = true;
+      while (hasMoreUsers) {
+        // Send the request to Duo and parse the response.
+        System.out.println("Fetching " + limit + " users at offset " + offset);
+
+        result = (JSONObject) request.executeJSONRequest();
+        response = result.getJSONArray("response");
+        metadata = result.getJSONObject("metadata");
+
+        if (!metadata.isNull("next_offset")) {
+          offset = metadata.getInt("next_offset");
+
+          request = new Http("GET", cmd.getOptionValue("host"), "/admin/v1/users");
+          request.addParam("offset", Integer.toString(offset));
+          request.addParam("limit", limit);
+          request.signRequest(cmd.getOptionValue("ikey"), cmd.getOptionValue("skey"));
+
+          System.out.println("More to fetch: next_offset = " + offset);
+        } else {
+          hasMoreUsers = false;
+          System.out.println("Fetch user request done.");
+        }
+
+        // iterate users and print them
+        for (int i = 0; i < response.length(); i++) {
+          JSONObject user = response.getJSONObject(i);
+          System.out.println("Fetched user: " + user.get("username"));
+        }
+      }
+    } catch (Exception e) {
+      System.out.println("error making request");
+      System.out.println(e.toString());
+    }
+  }
 }
