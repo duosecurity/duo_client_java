945942e16aae03bc544c7a482afe13909ddfb140
Yizhen Shi <61877973+yizshi@users.noreply.github.com>
2023-04-01 11:09:54 -0400

    Delete duo-client/src/main/java/com/duosecurity/client/Canonicalization directory

--- duo-client/src/main/java/com/duosecurity/client/Canonicalization/Canonicalizer.java
+++
@@ -1,5 +0,0 @@
-package com.duosecurity.client.canonicalization;
-
-public abstract class Canonicalizer {
-  public abstract String canonicalize(String method, String host, String url, String queryString);
-}
