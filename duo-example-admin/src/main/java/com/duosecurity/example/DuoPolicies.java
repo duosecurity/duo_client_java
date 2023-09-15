ec8d0e8070e64e8f015e54a38e555818136e0bc6
Jonathan Aherne <jaherne@cisco.com>
2023-09-15 08:18:59 -0700

    Add bulk policy example (#75)
    * Add bulk policy example
    
    * name -> policy_name
    
    * Fix linting
    
    * Update docstrings

--- duo-example-admin/src/main/java/com/duosecurity/example/DuoPolicies.java
+++ duo-example-admin/src/main/java/com/duosecurity/example/DuoPolicies.java
@@ -7,6 +7,7 @@ package com.duosecurity.example;
  */
 
 import com.duosecurity.client.Admin;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.commons.cli.CommandLine;
@@ -79,12 +80,17 @@ public class DuoPolicies {
     }
 
     printGlobalPolicy(cmd);
-    createNewPolicy(cmd);
+    String newPolicyKey = createNewPolicy(cmd);
+    copyPolicy(cmd, newPolicyKey);
     printAllPolicies(cmd);
   }
 
+  /**
+   * Print the global policy to stdout.
+   * 
+   * @param cmd the command line arguments
+   */
   private static void printGlobalPolicy(CommandLine cmd) {
-    // Print the global policy to stdout.
     System.out.println("Getting Global Policy");
     try {
       Admin adminRequest = new Admin.AdminBuilder(
@@ -106,15 +112,20 @@ public class DuoPolicies {
     }
   }
 
-  private static void createNewPolicy(CommandLine cmd) {
-    // Create a new policy and print it to stdout.
+  /**
+   * Create a new policy and print it to stdout.
+   * 
+   * @param cmd the command line arguments
+   * @return the new policy's policy_key
+   */
+  private static String createNewPolicy(CommandLine cmd) {
     System.out.println("Creating New Policy");
     try {
       Admin adminRequest = new Admin.AdminBuilder(
           "POST",
           cmd.getOptionValue("host"),
           "/admin/v2/policies").build();
-      adminRequest.addParam("name", "New Sample Policy");
+      adminRequest.addParam("policy_name", "New Sample Policy");
       adminRequest.addParam("sections",
           new JSONObject()
               .put("authentication_methods",
@@ -139,14 +150,54 @@ public class DuoPolicies {
 
       JSONObject result = (JSONObject) adminRequest.executeJSONRequest();
       System.out.println(result.toString(4));
+      return result.getJSONObject("response").getString("policy_key");
+    } catch (Exception e) {
+      System.out.println("Error making request");
+      System.out.println(e.toString());
+      return null;
+    }
+  }
+
+  /**
+   * Copy an existing policy twice and print to stdout.
+   * 
+   * @param cmd           the command line arguments
+   * @param policyToCopy  the policy_key to copy
+   */
+  private static void copyPolicy(CommandLine cmd, String policyToCopy) {
+    System.out.println("Copying policy");
+    try {
+      Admin adminRequest = new Admin.AdminBuilder(
+          "POST",
+          cmd.getOptionValue("host"),
+          "/admin/v2/policies/copy").build();
+      adminRequest.addParam("policy_key", policyToCopy);
+      adminRequest.addParam("new_policy_names_list", new ArrayList<Object>() {
+        {
+          add("New Copied Policy 1");
+          add("New Copied Policy 2");
+        }
+      });
+      adminRequest.signRequest(cmd.getOptionValue("ikey"), cmd.getOptionValue("skey"));
+
+      // optional proxy
+      if (proxy_host != null) {
+        adminRequest.setProxy(proxy_host, proxy_port);
+      }
+      JSONObject result = (JSONObject) adminRequest.executeJSONRequest();
+      System.out.println(result.toString(4));
     } catch (Exception e) {
       System.out.println("Error making request");
       System.out.println(e.toString());
     }
   }
 
+  /**
+   * Page through all policies and print the names to stdout.
+   * 
+   * @param cmd the command line arguments
+   */
   private static void printAllPolicies(CommandLine cmd) {
-    // Page through all policies and print the names to stdout.
     System.out.println("Printing All Policies");
     try {
       int limit = 10;
@@ -177,7 +228,7 @@ public class DuoPolicies {
           System.out.println(
               String.format(
                   "Policy w/ name: \"%s\"",
-                  policies.getJSONObject(x).getString("name")));
+                  policies.getJSONObject(x).getString("policy_name")));
         }
 
         // If next_offset doesn't exist we have reached the end of the dataset.
